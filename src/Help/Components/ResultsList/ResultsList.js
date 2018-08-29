a26cb5eacc8f3d50ab2071d220bea37c83b75f6e
Richard Ellis <info@lookstechnical.co.uk>
2018-08-29 12:27:44 +0100

    add feedback

--- src/Help/Components/ResultsList/ResultsList.js
+++ src/Help/Components/ResultsList/ResultsList.js
@@ -43,11 +43,15 @@ const ResultsListItem = ({page}) =>  (
 )
 
 const ResultsList = (props) =>  (
-    <List>
-        {props.help.paginator.data.map((page, index) => (
-            <ResultsListItem key={index} page={page}/>
-        ))}
-    </List>
+    <div>
+        <h4>Results for {props.help.query}</h4>
+        <p>{props.help.paginator.page} of {props.help.paginator.total_pages}</p>
+        <List>
+            {props.help.paginator.data.map((page, index) => (
+                <ResultsListItem key={index} page={page}/>
+            ))}
+        </List>
+    </div>
 )
 
 const enhance = compose(
